e3798aa8c523f892d0fd78af3254e4d93e3477fc
Md Asif <asifaftab87@gmail.com>
2017-10-24 15:33:07 +0800

    Adding constants class.

---
+++ src/main/java/org/asif/constants/Constants.java
@@ -0,0 +1,6 @@
+package org.asif.constants;
+
+public class Constants {
+
+	public static final String HOME									= "home";
+}
--- src/main/java/org/asif/web/HomeController.java
+++ src/main/java/org/asif/web/HomeController.java
@@ -1,16 +1,17 @@
 package org.asif.web;
 
+import org.asif.constants.Constants;
 import org.asif.constants.StringPool;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller															//Declared to be a controller
+@Controller																				//Declared to be a controller
 public class HomeController {
 
 	@RequestMapping(value=StringPool.FORWARD_SLASH, method=RequestMethod.GET)			//Handle GET request for
 	public String home(){
-		return "home";												//View name is home
+		return Constants.HOME;															//View name is home
 	}
 	
 }
